4d7a88fc59b1bef889d3d763b261454242d2ed62
unknown <daniel.bartulec01@estudiant.upf.edu>
2024-04-02 15:59:56 +0200

    added one pdb file and changed the project file

--- projectPYTSBI.py
+++ projectPYTSBI.py
@@ -3,27 +3,31 @@ from Bio.PDB import Selection
 
 # Load PDB file
 pdb_parser = PDBParser()
-structure = pdb_parser.get_structure("PHA-L", "1fat.pdb")
-
-
+structure = pdb_parser.get_structure("PHA-L", "7ofk.pdb")
+atoms = []
+hetatoms = []
 
 for model in structure:
     for chain in model:
         for residue in chain:
-            for atom in residue:
-                print(atom)
+            if residue.id[0] == ' ' and residue.resname != 'HOH':  # Ignore water molecules
+                atoms.append(residue)
+            elif residue.id[0][0:2] == 'H_':
+                if len(residue) > 5:  # Check for large HETATM
+                    hetatoms.append(residue)
+print(hetatoms)
 
-
+"""
 # Extract all residues with a ligand
 ligand_residues = Selection.unfold_entities(structure, "R")
-
+print(ligand_residues)
 # You can then perform further analysis or prediction on the ligand residues
 # For example, identifying ligand binding sites based on certain criteria
 # or using machine learning algorithms
 
 # Here's a simple example:
 # Assume you want to find ligand residues within a certain distance threshold from a metal ion
-metal_ion_coord = (x, y, z)  # Coordinates of the metal ion
+metal_ion_coord = (0,0,0)  # Coordinates of the metal ion
 distance_threshold = 5.0  # Threshold distance in angstroms
 
 ligand_binding_sites = []
@@ -38,3 +42,4 @@ for residue in ligand_residues:
 
 # Now ligand_binding_sites contains residues within the specified distance from the metal ion
 # You can further analyze these residues as needed
+"""
