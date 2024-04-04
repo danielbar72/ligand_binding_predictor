ac2cf5827ce8ae9d614dc3a1ce254778aacc0723
Lorenavc <146228656+Lorenavc@users.noreply.github.com>
2024-04-04 18:35:27 +0200

    Update pockets.py

--- pockets.py
+++ pockets.py
@@ -1,7 +1,5 @@
 import os
 import subprocess
-from Bio.PDB import PDBParser  
-
 
 def find_pockets(pdb_file):
     # Run fpocket command to generate pocket files
@@ -10,28 +8,27 @@ def find_pockets(pdb_file):
     # Get the path to the fpocket output directory
     output_dir = os.path.splitext(pdb_file)[0] + '_out'
 
-    # Iterate over the pocket files in the output directory
-    for file_name in os.listdir(output_dir):
-        if file_name.endswith('.pdb'):
-            pocket_num = file_name.split('.')[0].split('_')[-1]
-            pocket_residues = set()  # Use a set to avoid duplicate residues
-            
-            file_name = os.path.join(output_dir, file_name)
+    # Get the path to the pockets directory
+    pockets_dir = os.path.join(output_dir, 'pockets')
 
-            parser = PDBParser()
-            structure = parser.get_structure("file", file_name)
-            for r in structure.get_residues():
-                print(r.id)
-                
-            """
+    # Iterate over the pocket files in the pockets directory
+    for file_name in os.listdir(pockets_dir):
+        if file_name.endswith('.pdb'):
+            pocket_num = file_name.split('.')[0].split('_')[0]
+            pocket_residues = set()  # Use a set to avoid duplicate residues
+
             # Read the pocket file and extract the residues
-            with open(os.path.join(output_dir, file_name), 'r') as pocket_file:
+            with open(os.path.join(pockets_dir, file_name), 'r') as pocket_file:
                 for line in pocket_file:
                     if line.startswith('ATOM'):
                         residue = line[17:20].strip()
                         residue_id = line[22:26].strip()
                         full_residue = f"{residue}{residue_id}"
                         pocket_residues.add(full_residue)  # Add residues to set
-            """
+
             # Output the pocket number and the residues involved
-            #print(f"Residues in pocket {pocket_num}: {', '.join(pocket_residues)}")
+            print(f"Residues in pocket {pocket_num}: {', '.join(pocket_residues)}")
+
+# Example usage
+#pdb_file = "path/to/your/pdb/file.pdb"
+#find_pockets(pdb_file)
