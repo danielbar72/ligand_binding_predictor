3bec85d5ec348391f6816b779a86978b087af600
Lorenavc <146228656+Lorenavc@users.noreply.github.com>
2024-04-04 18:23:51 +0200

    Update pockets.py

--- pockets.py
+++ pockets.py
@@ -1,28 +1,37 @@
+import os
 import subprocess
-from collections import defaultdict
+from Bio.PDB import PDBParser  
 
-def run_fpocket(pdb_file):
-    try:
-        # Run fpocket command
-        result = subprocess.run(['fpocket', '-f', pdb_file], capture_output=True, text=True, check=True)
 
-        # Process the output
-        pockets = defaultdict(list)
-        current_pocket_id = None
-        for line in result.stdout.split("\n"):
-            if line.startswith("Pocket"):
-                current_pocket_id = int(line.split(":")[1].strip())
-            elif line.startswith("Residue"):
-                residue_info = line.split(":")[1].strip()
-                residue_chain, residue_id = residue_info.split("_")
-                pockets[current_pocket_id].append((residue_chain, residue_id))
+def find_pockets(pdb_file):
+    # Run fpocket command to generate pocket files
+    subprocess.run(['fpocket', '-f', pdb_file])
 
-        return pockets
+    # Get the path to the fpocket output directory
+    output_dir = os.path.splitext(pdb_file)[0] + '_out'
 
-    except subprocess.CalledProcessError as e:
-        # fpocket command failed, raise custom exception with error message
-        raise Exception(f"fpocket failed with error: {e.stderr}")
+    # Iterate over the pocket files in the output directory
+    for file_name in os.listdir(output_dir):
+        if file_name.endswith('.pdb'):
+            pocket_num = file_name.split('.')[0].split('_')[-1]
+            pocket_residues = set()  # Use a set to avoid duplicate residues
+            
+            file_name = os.path.join(output_dir, file_name)
 
-    except Exception as e:
-        # Other exceptions (e.g., subprocess.run error), raise with original message
-        raise e
+            parser = PDBParser()
+            structure = parser.get_structure("file", file_name)
+            for r in structure.get_residues():
+                print(r.id)
+                
+            """
+            # Read the pocket file and extract the residues
+            with open(os.path.join(output_dir, file_name), 'r') as pocket_file:
+                for line in pocket_file:
+                    if line.startswith('ATOM'):
+                        residue = line[17:20].strip()
+                        residue_id = line[22:26].strip()
+                        full_residue = f"{residue}{residue_id}"
+                        pocket_residues.add(full_residue)  # Add residues to set
+            """
+            # Output the pocket number and the residues involved
+            #print(f"Residues in pocket {pocket_num}: {', '.join(pocket_residues)}")
