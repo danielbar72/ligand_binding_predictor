104282561206847eff302faaf2bd2999927e6a51
Lorenavc <146228656+Lorenavc@users.noreply.github.com>
2024-04-05 15:06:24 +0200

    Add files via upload

---
+++ color_pdb.py
@@ -0,0 +1,26 @@
+from Bio.PDB import PDBParser, PDBIO
+from Bio.PDB.Color import Color
+
+# Load the PDB file
+parser = PDBParser()
+structure = parser.get_structure("protein", "input.pdb")
+highlighted_residues = [] 
+# Define the residues to be highlighted
+predicted_sites = predict_binding_sites(pdb_input_file, model)
+for pocket_num, residues in predicted_sites.items():
+        if pocket_num == 100:
+             for r in residues:
+                highlighted_residues.append(r[1])
+
+
+# Color the residues
+for model in structure:
+    for chain in model:
+        for residue in chain:
+            if residue.id[1] in highlighted_residues:
+                residue.color = Color(1, 0, 0)  # Red
+
+# Save the modified PDB file
+io = PDBIO()
+io.set_structure(structure)
+io.save("output.pdb")
